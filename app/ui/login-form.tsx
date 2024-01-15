c9d7b426d22c8aebdca6964ccf38651668046872
Manas <manasm.dev@gmail.com>
2024-01-15 19:42:22 +1000

    Updated UI

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -17,7 +17,7 @@ export default function LoginForm() {
   return (
     <form action={dispatch} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={`${lusitana.className} flex justify-center items-center mb-3 text-2xl`}>
+        <h1 className="flex justify-center items-center mb-3 text-2xl">
           Login
         </h1>
         <div className="w-full">

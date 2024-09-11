d828b4436c0c0cad78e72229477aaee6e5063bbc
Manas <manasm.dev@gmail.com>
2024-09-11 17:27:45 +1000

    fixed error

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -20,7 +20,7 @@ export type Invoice = {
   id: string;
   customer_id: string;
   amount: number;
-  date: string;
+  invoice_date: string;
   // In TypeScript, this is called a string union type.
   // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
   status: 'pending' | 'paid';
@@ -50,7 +50,7 @@ export type InvoicesTable = {
   name: string;
   email: string;
   image_url: string;
-  date: string;
+  invoice_date: string;
   amount: number;
   status: 'pending' | 'paid';
 };
@@ -84,7 +84,7 @@ export type InvoiceForm = {
   id: string;
   customer_id: string;
   amount: number;
-  date: string;
+  invoice_date: string;
   status: 'pending' | 'paid';
 };
 
